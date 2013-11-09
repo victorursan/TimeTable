9a0bfcd2c2425f96c4e1d67d475a65f56f685462
Victor Ursan <victor.ursan@gmail.com>
2013-11-09 23:03:00 +0200

    added a tableView in MainViewController

--- TimeTable/MainViewController.m
+++ TimeTable/MainViewController.m
@@ -15,21 +15,49 @@
 @implementation MainViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+  }
+  return self;
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
+  [super viewDidLoad];
+  self.title = @"Wednesday";
+  [self addTableView];
+
+  self.navigationController.navigationBar.barTintColor = [UIColor orangeColor];
+  self.navigationController.navigationBar.barStyle = UIBarStyleBlackOpaque;
+}
+
+- (void)addTableView {
+  self.tableView = [[UITableView alloc] initWithFrame:self.view.frame];
+  self.tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+  self.tableView.delegate = self;
+  self.tableView.dataSource = self;
+  self.tableView.allowsSelection = NO;
+  [self.view addSubview:self.tableView];
+
+}
+#pragma mark - Table view
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  static NSString *CellIdentifier = @"Cell";
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    cell.accessoryType = UITableViewCellAccessoryNone;
+  }
+  cell.textLabel.text= @"hello";
+  return cell;
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 @end
