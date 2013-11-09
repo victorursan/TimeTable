80dd4cb68ae405d04a759fe576bee00de146690c
Victor Ursan <victor.ursan@gmail.com>
2013-11-09 16:55:57 +0200

    Added MainViewController

---
+++ TimeTable/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  TimeTable
+//
+//  Created by Victor Ursan on 11/9/13.
+//  Copyright (c) 2013 Victor Ursan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ TimeTable/MainViewController.m
@@ -0,0 +1,35 @@
+//
+//  MainViewController.m
+//  TimeTable
+//
+//  Created by Victor Ursan on 11/9/13.
+//  Copyright (c) 2013 Victor Ursan. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
